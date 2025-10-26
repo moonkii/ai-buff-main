a692b42e2a7a2757ddf91523f985122388d66974
noah <noahlog@naver.com>
2025-10-26 16:20:35 +0900

    피드백 반영

--- ai-buff/js/main.js
+++ ai-buff/js/main.js
@@ -114,7 +114,7 @@ $(document).ready(function () {
   });
 
   // section-05
-  const section05Swiper = new Swiper('.section-05-swiper', {
+  new Swiper('.section-05-swiper', {
     loop: true,
     autoplay: {
       delay: 2000,
@@ -123,7 +123,30 @@ $(document).ready(function () {
     speed: 1500,
     slidesPerView: 1,
     spaceBetween: 20,
+    allowTouchMove: false,
   });
+
+  const section05Animation = gsap.timeline({
+    scrollTrigger: {
+      trigger: '.section-05-mobile',
+      pin: false,
+      start: 'top top+=250',
+      end: 'bottom bottom',
+    },
+  });
+
+  section05Animation
+    .to('.section-05-animation', {
+    opacity: 1,
+    ease: 'power2.inOut',
+    duration: 0.5,
+  }).to('.section-05-animation', {
+    scale: 1.1,
+    duration: 0.7,
+    repeat: 3,
+    yoyo: true,
+    ease: 'power2.inOut',
+  }, 0.3);
 
   // section-06
 
